e2fa68ea914d77a9f0ff7ae7fc46602346493779
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-08-31 18:44:52 +0100

    Created Dummy component

---
+++ src/components/Dummy.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Caption from "./Caption";
+import Summary from "./Summary";
+
+const Dummy = () => {
+	return (
+		<div style={{ width: "50%" }}>
+			<Caption>Dummy Content</Caption>
+			<Summary>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Summary>
+		</div>
+	);
+}
+
+export default Dummy;
